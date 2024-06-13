876da0b922b6d26820935e2281eb64dbd36b0f4b
Lyytimmy <Lyy2500673659@88.com>
2024-06-13 17:34:46 +0800

    feat(DynamicThreadPoolService): 采集线程池的配置数据
    1.设置配置入口
    2.写服务接口
    3.实现查询所有线程池信息服务
    4.实现根据名字查询线程池信息服务
    5.实现更新线程池服务
    6.创建线程池配置信息实体类

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/cn/lyy/dynmaic/thread/pool/sdk/domain/service/DynamicThreadService.java
@@ -0,0 +1,26 @@
+package cn.lyy.dynmaic.thread.pool.sdk.domain.service;
+
+import cn.lyy.dynmaic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+
+import java.util.List;
+
+/**
+ * 动态线程池服务
+ */
+public interface DynamicThreadService {
+    /**
+     * 查询所有线程池信息
+     */
+    List<ThreadPoolConfigEntity> queryThreadPoolList();
+
+    /**
+     * 根据名字查线程池信息
+     */
+    ThreadPoolConfigEntity queryThreadPoolConfigByName(String threadPoolName);
+
+    /**
+     * 更新线程池信息
+     * @param threadPoolConfigEntity
+     */
+    void updateThreadPoolConfig(ThreadPoolConfigEntity threadPoolConfigEntity);
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/cn/lyy/dynmaic/thread/pool/sdk/domain/service/Imp/DynamicThreadServiceImpl.java
@@ -0,0 +1,78 @@
+package cn.lyy.dynmaic.thread.pool.sdk.domain.service.Imp;
+
+import cn.lyy.dynmaic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import cn.lyy.dynmaic.thread.pool.sdk.domain.service.DynamicThreadService;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+public class DynamicThreadServiceImpl implements DynamicThreadService {
+    private final Logger logger = LoggerFactory.getLogger(DynamicThreadServiceImpl.class);
+    private final String applicationName;
+    private final Map<String, ThreadPoolExecutor> threadPoolExecutorMap;
+
+    public DynamicThreadServiceImpl(String applicationName,Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+        this.applicationName = applicationName;
+        this.threadPoolExecutorMap = threadPoolExecutorMap;
+    }
+
+    @Override
+    public List<ThreadPoolConfigEntity> queryThreadPoolList() {
+        Set<String> threadPoolBeanNames = threadPoolExecutorMap.keySet();
+        List<ThreadPoolConfigEntity> threadPoolVOS = new ArrayList<>(threadPoolBeanNames.size());
+        for (String beanName : threadPoolBeanNames) {
+            ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(beanName);
+            ThreadPoolConfigEntity threadPoolConfigVO = new ThreadPoolConfigEntity(applicationName, beanName);
+            threadPoolConfigVO.setCorePoolSize(threadPoolExecutor.getCorePoolSize());
+            threadPoolConfigVO.setMaximumPoolSize(threadPoolExecutor.getMaximumPoolSize());
+            threadPoolConfigVO.setActiveCount(threadPoolExecutor.getActiveCount());
+            threadPoolConfigVO.setPoolSize(threadPoolExecutor.getPoolSize());
+            threadPoolConfigVO.setQueueType(threadPoolExecutor.getQueue().getClass().getSimpleName());
+            threadPoolConfigVO.setQueueSize(threadPoolExecutor.getQueue().size());
+            threadPoolConfigVO.setRemainingCapacity(threadPoolExecutor.getQueue().remainingCapacity());
+            threadPoolVOS.add(threadPoolConfigVO);
+        }
+        return threadPoolVOS;
+    }
+
+    @Override
+    public ThreadPoolConfigEntity queryThreadPoolConfigByName(String threadPoolName) {
+        ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolName);
+        if (threadPoolExecutor == null) return new ThreadPoolConfigEntity(applicationName, threadPoolName);
+
+        // 线程池配置数据
+        ThreadPoolConfigEntity threadPoolConfigVO = new ThreadPoolConfigEntity(applicationName, threadPoolName);
+        threadPoolConfigVO.setCorePoolSize(threadPoolExecutor.getCorePoolSize());
+        threadPoolConfigVO.setMaximumPoolSize(threadPoolExecutor.getMaximumPoolSize());
+        threadPoolConfigVO.setActiveCount(threadPoolExecutor.getActiveCount());
+        threadPoolConfigVO.setPoolSize(threadPoolExecutor.getPoolSize());
+        threadPoolConfigVO.setQueueType(threadPoolExecutor.getQueue().getClass().getSimpleName());
+        threadPoolConfigVO.setQueueSize(threadPoolExecutor.getQueue().size());
+        threadPoolConfigVO.setRemainingCapacity(threadPoolExecutor.getQueue().remainingCapacity());
+
+        if (logger.isDebugEnabled()) {
+            logger.info("动态线程池，配置查询 应用名:{} 线程名:{} 池化配置:{}", applicationName, threadPoolName, JSON.toJSONString(threadPoolConfigVO));
+        }
+
+        return threadPoolConfigVO;
+    }
+
+    @Override
+    public void updateThreadPoolConfig(ThreadPoolConfigEntity threadPoolConfigEntity) {
+        // 判断要更新的线程池是不是本设备的
+        if (threadPoolConfigEntity == null || !applicationName.equals(threadPoolConfigEntity.getAppName())) return;
+        ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolConfigEntity.getThreadPoolName());
+        if (threadPoolExecutor == null) return;
+
+        // 设置参数 「调整核心线程数和最大线程数」
+        threadPoolExecutor.setCorePoolSize(threadPoolConfigEntity.getCorePoolSize());
+        threadPoolExecutor.setMaximumPoolSize(threadPoolConfigEntity.getMaximumPoolSize());
+    }
+}
